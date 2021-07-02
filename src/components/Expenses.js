38adf29c62a68e27cdbd7dfad2a001c90e958bae
Will-Gon <will.colocho@gmail.com>
2021-07-02 14:15:11 -0700

    add components

---
+++ src/components/Expenses.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem'
+import Card from './Card';
+import './Expenses.css'
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            {props.expenses.map(item => {
+                return (
+                    <ExpenseItem 
+                    title={item.title}
+                    date={item.date}
+                    amount={item.amount}
+                    />
+                );
+            })};
+        </Card>
+    )
+};
+
+export default Expenses;
