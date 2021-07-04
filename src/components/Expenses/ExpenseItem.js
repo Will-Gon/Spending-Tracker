cd298049d9f624452d780b9d38279b7d3d2b987d
Will-Gon <will.colocho@gmail.com>
2021-07-03 19:20:35 -0700

    clean up code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,14 +8,13 @@ const ExpenseItem = (props) => {
 
     const clickHandler = () => {
         setTitle('Updated!');
-        //console.log(title);
     };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
         </Card>
